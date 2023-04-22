0acfbadf032d1ae1a11ad74c5dd836d3207dd7f0
asharamanujam <103427149+asharamanujam@users.noreply.github.com>
2023-04-22 01:00:42 -0400

    Added install

---
+++ Install.jl
@@ -0,0 +1,19 @@
+import Pkg
+Pkg.add("DataFrames")
+import Pkg
+Pkg.add("CSV")
+import Pkg
+Pkg.add("Statistics")
+import Pkg
+Pkg.add("Clustering")
+import Pkg
+Pkg.add("FileIO")
+
+import Pkg
+Pkg.add("JuMP")
+
+import Pkg
+Pkg.add("Random")
+
+import Pkg
+Pkg.add("Dates")
