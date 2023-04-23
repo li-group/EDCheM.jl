2460f56eff9f4a7910cfcdefeb78e096d048e672
Can Li <canli@dantzig.ecn.purdue.edu>
2023-04-23 11:32:35 -0400

    change install.jl

--- Install.jl
+++ Install.jl
@@ -1,19 +1,9 @@
 import Pkg
 Pkg.add("DataFrames")
-import Pkg
 Pkg.add("CSV")
-import Pkg
 Pkg.add("Statistics")
-import Pkg
 Pkg.add("Clustering")
-import Pkg
 Pkg.add("FileIO")
-
-import Pkg
 Pkg.add("JuMP")
-
-import Pkg
 Pkg.add("Random")
-
-import Pkg
 Pkg.add("Dates")
